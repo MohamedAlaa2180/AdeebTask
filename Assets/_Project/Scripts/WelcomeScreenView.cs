d6b23c5e8a301a5aaa55254c1da9d5531a93cf6e
Mohamed Alaa <32314285+MohamedAlaa2180@users.noreply.github.com>
2025-03-08 16:35:23 +0200

    Add logout

--- Assets/_Project/Scripts/WelcomeScreenView.cs
+++ Assets/_Project/Scripts/WelcomeScreenView.cs
@@ -1,12 +1,34 @@
+using Firebase;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WelcomeScreenView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI welcomeText;
+    [SerializeField] private TextMeshProUGUI idText;
+    [SerializeField] private Button logoutButton;
+
     private void Start()
     {
         welcomeText.text = $"Welcome, {SessionData.CurrentUser.Name}!";
+        idText.text = $"ID: {SessionData.CurrentUser.UserId}";
+    }
 
+    private void OnEnable()
+    {
+        logoutButton.onClick.AddListener(OnLogoutButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        logoutButton.onClick.RemoveListener(OnLogoutButtonClicked);
+    }
+
+    private void OnLogoutButtonClicked()
+    {
+        FirebaseManager.LogoutUser();
+        SceneManager.LoadScene("LoginRegisterScene");
     }
 }
